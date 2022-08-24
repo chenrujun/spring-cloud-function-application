5a9ef576075d2f36475fee974e202bd5502b18d1
Rujun Chen <rujche@microsoft.com>
2022-08-24 13:16:11 +0800

    1. Update .gitignore:  Add "**/application-develop.yml". 2. Add a feature: Use BlobClient to read and write contents in blob. 3. Add application.yml 4. Update README.md: add curl command to access bolb related endpoint.

--- README.md
+++ README.md
@@ -4,19 +4,33 @@ This is used to investigate deploying Spring Cloud Function application to Azure
 
 # Run this application
 
+## Run this application in localhost
+
 This is a Spring boot application. You can run it locally. Then access endpoints by these commands:
 
-1. Function
-```shell
-curl -s -d test -X POST http://localhost:8080/uppercase 
-```
-
-2. Consumer
-```shell
-curl -s -d test -X POST http://localhost:8080/consumer
-```
-
-3. Supplier
+1. Supplier
 ```shell
 curl -s http://localhost:8080/supplier
+```
+
+2. Function
+```shell
+curl -s -d 'key=value' -X POST http://localhost:8080/uppercase 
+```
+
+3. Consumer
+```shell
+curl -s -d 'key=value' -X POST http://localhost:8080/consumer
+```
+
+4. Read blob
+```shell
+curl -s http://localhost:8080/readBlob
+```
+
+5. Write blob
+```shell
+timestamp=$(date +%T)
+echo "timestamp=$timestamp"
+curl -s -d "timestamp=$timestamp" http://localhost:8080/writeBlob
 ```
--- src/main/java/chenrujun/spring/cloud/function/application/Application.java
+++ src/main/java/chenrujun/spring/cloud/function/application/Application.java
@@ -1,11 +1,17 @@
 package chenrujun.spring.cloud.function.application;
 
+import com.azure.storage.blob.BlobClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
-import reactor.core.publisher.Flux;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -13,22 +19,52 @@ import java.util.function.Supplier;
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    BlobClient blobClient;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
-    public Function<Flux<String>, Flux<String>> uppercase() {
-        return flux -> flux.map(String::toUpperCase);
+    public Function<String, String> uppercase() {
+        return (String string) -> {
+            LOGGER.info("uppercase function executed. string = {}", string);
+            return string.toUpperCase();
+        };
     }
 
     @Bean
     public Supplier<String> supplier() {
-        return () -> "test";
+        return () -> {
+            String suppliedValue = "This is a string supplied by supplier. Date = " + new Date();
+            LOGGER.info("supplier executed. suppliedValue = {}", suppliedValue);
+            return suppliedValue;
+        };
     }
 
     @Bean
     public Consumer<String> consumer() {
-        return System.out::println;
+        return (String string) -> LOGGER.info("consumer executed. string = {}", string);
+    }
+
+    @Bean
+    public Supplier<String> readBlob() {
+        return () -> {
+            String blobContent = blobClient.downloadContent().toString();
+            LOGGER.info("readBlob executed. blobContent = {}", blobContent);
+            return blobContent;
+        };
+    }
+
+    @Bean
+    public Consumer<String> writeBlob() {
+        return (String string) -> {
+            LOGGER.info("writeBlob executed. string = {}", string);
+            InputStream dataStream = new ByteArrayInputStream(string.getBytes(StandardCharsets.UTF_8));
+            blobClient.upload(dataStream, string.length(), true);
+        };
     }
 }
